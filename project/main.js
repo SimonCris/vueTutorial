4ad5734579ed305a0f8f82d458fd288e70700400
Simone Crisafi <simone.crisafi@nttdata.com>
2023-04-27 12:07:05 +0200

    update styles

--- project/main.js
+++ project/main.js
@@ -2,14 +2,19 @@ var App = Vue.component('App', {
     template: `
         <div class="container">
             <h1> {{ titolo }} </h1>
-            <p>{{ 5+3 }}</p>
+            <p> <span :style = "colorRed"> Totale: </span>{{ 5+3 }} </p>
             <p>{{ user.firstName }} {{ user.secondName }}</p>
             <p>{{ user.getFullName() }}</p>
+
+            <button class="btn btn-primary" :disabled="isDisabled"> Click </button>
+            <p>Bottone disabilitato: {{ isDisabled ? 'Si' : 'No' }}</p>
         </div>
     `,
     data() {
         return {
             titolo: "Ciao a tutti!",
+            colorRed: "color: red",
+            isDisabled: false,
             user: {
                 firstName: "Mario",
                 secondName: "Rossi",
